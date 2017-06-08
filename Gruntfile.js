56eea0e4fe839fa6d067e967b2e44b91fa8cd47e
simssons <simssons@naver.com>
2017-06-08 23:29:09 +0900

    0608 home

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,13 @@
 module.exports = function(grunt) {
 
   // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+  grunt.initConfig({//가. 프로젝트환경설정
+    pkg: grunt.file.readJSON('package.json'),//플러그인 설정
     serve: {
         options: {
             port: 9000
         }
-    },
+    },//less 플러그인 설정
     less: {
         product: {//여기서 product(사용자에게 보여줄)와 development(개발자만보면되는)는 맘대로 작명.
             options: {
@@ -45,6 +45,7 @@ module.exports = function(grunt) {
  grunt.loadNpmTasks('grunt-serve');
  grunt.loadNpmTasks('grunt-contrib-less');
  grunt.loadNpmTasks('grunt-contrib-watch');
+ 
 
   // Default task(s).
   grunt.registerTask('default', ['serve']);
