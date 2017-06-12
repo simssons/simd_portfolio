2eaeff5964703612ad8fa08142f113a64e5310b3
Jaehun Sim <simssons@naver.com>
2017-06-12 18:21:25 +0900

    6월21일 학원

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,6 +37,17 @@ module.exports = function(grunt) {
         },
       },
     },
+    includes: { //복수로 여러개
+      dist:{
+          cwd:'html', //current working directory 현재경로
+          src:['*.html','includes/*.html'], //대상파일들
+          dest:'./dist', //destination 만들어진 파일이 들어갈 경로. 목적지
+          options:{
+              flatten:true,
+              includePath:'html/includes/' //Indicates the path(s) to use when looking for included files.포함된 파일들 찾을때 사용할 경로.
+          }
+      }
+    },
 
 
   });
@@ -45,7 +56,7 @@ module.exports = function(grunt) {
  grunt.loadNpmTasks('grunt-serve');
  grunt.loadNpmTasks('grunt-contrib-less');
  grunt.loadNpmTasks('grunt-contrib-watch');
- 
+ grunt.loadNpmTasks('grunt-includes');
 
   // Default task(s).
   grunt.registerTask('default', ['serve']);
