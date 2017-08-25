1a6ebe787aff51e2d890c02a3cab453cb26a2b6a
simssons <simssons@naver.com>
2017-08-25 16:54:22 +0900

    gnb 수정

--- assets/javascript/common.js
+++ assets/javascript/common.js
@@ -3,8 +3,8 @@ $(function(){
     //menu관련
     var autoHeight = function(){
         $('.menu-main-menu-container .fifty').height(($('.menu-main-menu-container').height()-90)/2)
-
     }
+
     $(window).resize(function(){
         autoHeight();
             console.log($('.menu-main-menu-container').height());
@@ -17,9 +17,9 @@ $(function(){
         var $blogList = $('body.blog_list');
         var $aColor = $('.gnb-inner div a');
 
-
         if ($menuToggle.is('.on')) {
             $menuToggle.removeClass('on');
+            $('html').css('overflow-y', '')
             $(this).text('menu');
             if ($('html').has('.menuColor')) {
                 $aColor.css('color','');
@@ -30,6 +30,16 @@ $(function(){
             }
         }else{
             $menuToggle.addClass('on');
+            $('.gnb-inner div.share ul .share_facebook').css({
+              'background-image': 'url(../assets/image/facebook-icon-white.svg)'
+            });
+            $('.gnb-inner div.share ul .share_twitter').css({
+              'background-image': 'url(../assets/image/twitter-icon-white.svg)'
+            });
+            $('.gnb-inner div.share ul .share_google_plus').css({
+              'background-image': 'url(../assets/image/g-plus-icon-white.svg)'
+            });
+            $('html').css('overflow-y', 'hidden');
             $(this).text('back');
             $aColor.css('color','white');
         }
